ac1058cf8ea57fba86a7c4429f420c47d32127f6
DuduDev <107651217+eduardomendes117@users.noreply.github.com>
2024-06-22 18:55:49 -0300

    update

---
+++ public/scripts/app.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    const menubtn = document.querySelector(".menu-icon");
+    const menu = document.querySelector(".menu");
+    const nav = document.querySelector(".nav");
+    const close = document.querySelector(".close");
+
+    menu.addEventListener("click", () => nav.classList.toggle("active"));
+    close.addEventListener("click", () => nav.classList.remove("active"));
+
+    menubtn.addEventListener('click', function() {
+
+        if (menu.style.display === 'none') {
+            menu.style.display = 'block';
+            close.style.display = 'none';
+        } else {
+            menu.style.display = 'none';
+            close.style.display = 'block';
+        }
+    });
+});
--- src/menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuButton = document.getElementById('menu-button');
-    const mobileMenu = document.getElementById('mobile-menu');
-  
-    menuButton.addEventListener('click', () => {
-      mobileMenu.classList.toggle('hidden');
-    });
-}); 
