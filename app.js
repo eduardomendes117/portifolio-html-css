5b67f6df3d27c55971f8036216938889ff65a0c7
DuduDev <107651217+eduardomendes117@users.noreply.github.com>
2024-07-19 17:16:16 -0300

    update

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    const menubtn = document.querySelector(".menu-icon");
     const menu = document.querySelector(".menu");
     const nav = document.querySelector(".nav");
     const close = document.querySelector(".close");
-    const links = document.querySelector(".links");
-    const links2 = document.querySelector(".links2");
-    const links3 = document.querySelector(".links3");
 
     menu.addEventListener("click", () => nav.classList.toggle("active"));
     close.addEventListener("click", () => nav.classList.remove("active"));
-    
-
-    document.querySelectorAll('.links, .links2, .links3').forEach(link => {
-        link.addEventListener('click', () => {
-            nav.classList.remove("active");
-            menu.style.display = 'block';
-            close.style.display = 'none';
-        });
-    });
 
 
     menubtn.addEventListener('click', function() {
