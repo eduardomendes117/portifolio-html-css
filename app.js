cccd02c7926bd509df563bff715ecd90495f60a1
DuduDev <107651217+eduardomendes117@users.noreply.github.com>
2024-07-23 17:16:35 -0300

    update

--- app.js
+++ app.js
@@ -2,19 +2,15 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const menu = document.querySelector(".menu");
     const nav = document.querySelector(".nav");
     const close = document.querySelector(".close");
+    const link1 = document.querySelector(".link1")
+    const link2 = document.querySelector(".link2")
+    const link3 = document.querySelector(".link3")
+    const link4 = document.querySelector(".link4")
 
     menu.addEventListener("click", () => nav.classList.toggle("active"));
-    close.addEventListener("click", () => nav.classList.remove("active"));
-
-
-    menubtn.addEventListener('click', function() {
-
-        if (menu.style.display === 'none') {
-            menu.style.display = 'block';
-            close.style.display = 'none';
-        } else {
-            menu.style.display = 'none';
-            close.style.display = 'block';
-        }
-    });
+    // close.addEventListener("click", () => nav.classList.remove("active"));
+    link1.addEventListener("click", () => nav.classList.remove("active"));
+    link2.addEventListener("click", () => nav.classList.remove("active"));
+    link3.addEventListener("click", () => nav.classList.remove("active"));
+    link4.addEventListener("click", () => nav.classList.remove("active"));
 });
