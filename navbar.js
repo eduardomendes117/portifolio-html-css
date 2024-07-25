bb6d18ce55171734b2470156ac411bbf913b26dc
DuduDev <107651217+eduardomendes117@users.noreply.github.com>
2024-07-25 12:12:17 -0300

    update

--- navbar.js
+++ navbar.js
@@ -42,18 +42,3 @@ const mobileNavbar = new MobileNavbar(
   ".nav-list li",
 );
 mobileNavbar.init();
-
-document.addEventListener("DOMContentLoaded", (event) => {
-  const menu = document.querySelector(".menu");
-  const nav = document.querySelector(".nav");
-  const close = document.querySelector(".close");
-  const link1 = document.querySelector(".link1")
-  const link2 = document.querySelector(".link2")
-  const link3 = document.querySelector(".link3")
-  const link4 = document.querySelector(".link4")
-
-  link1.addEventListener("click", () => nav.classList.remove("active"));
-  link2.addEventListener("click", () => nav.classList.remove("active"));
-  link3.addEventListener("click", () => nav.classList.remove("active"));
-  link4.addEventListener("click", () => nav.classList.remove("active"));
-});
